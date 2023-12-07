de46c9afb511eeb5f976ba2b5eeb78021876d2af
akmalrizaev <akmaljon.rizayev@gmail.com>
2023-12-07 20:12:36 +0500

    Reporting Errors with Exceptions

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -2,21 +2,21 @@ import { readFile, writeFile } from 'fs/promises';
 
 export class MessagesRepository {
   async findOne(id: string) {
-    const contents = await readFile('message.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
 
     return messages[id];
   }
 
   async findAll() {
-    const contents = await readFile('message.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
 
     return messages;
   }
 
   async create(content: string) {
-    const contents = await readFile('message.json', 'utf8');
+    const contents = await readFile('messages.json', 'utf8');
     const messages = JSON.parse(contents);
 
     const id = Math.floor(Math.random() * 999);
